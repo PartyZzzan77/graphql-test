ff5c13020a4aeb1a579983c8aa9e043e1a8e4aeb
Mitya <partyzan327@gmail.com>
2023-01-02 17:48:38 +0300

    feat: implement a root query

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const chalk = require('chalk');
 const graphqlHTTP = require('express-graphql');
+const schema = require('../Schema/schema.js');
 
 const PORT = 3000;
+const highlightedError = chalk.bgRed.white;
+const highlightedSuccess = chalk.bgGreen.whiteBright;
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(PORT, (err) => {
     err
-        ? console.log(chalk.bgRed(err.message))
-        : console.log(chalk.bgGreen.whiteBright(`Server start at ${PORT}`));
+        ? console.log(highlightedError(err.message))
+        : console.log(highlightedSuccess(`Server start at ${PORT}`));
 });
