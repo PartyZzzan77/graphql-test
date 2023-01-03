967a5b20237a097f89aebb844ff231808e87ee4e
Mitya <partyzan327@gmail.com>
2023-01-03 14:07:16 +0300

    feat: transfer wet data to the MongoBD database

--- server/app.js
+++ server/app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const chalk = require('chalk');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('../Schema/schema.js');
+const { db } = require('../models/movie.js');
+const dotenv = require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const highlightedError = chalk.bgRed.white;
 const highlightedSuccess = chalk.bgGreen.whiteBright;
 
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 const app = express();
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', (err) => {
+    console.log(highlightedError(`Connection error: ${err.message}`));
+});
+db.once('open', () => {
+    console.log(highlightedSuccess('Connected to DB'));
+});
 
 app.listen(PORT, (err) => {
     err
